7ccfb33c12379c6b4263095c582fa1382b80b547
Iyare Usen <iyare.usen@gmail.com>
2024-12-17 16:50:59 +0100

    generated requirements text

---
+++ app.py
@@ -0,0 +1,138 @@
+import streamlit as st
+import machine_learning as ml
+import feature_extraction as fe
+from bs4 import BeautifulSoup
+import requests as re
+import matplotlib.pyplot as plt
+from urllib3.exceptions import LocationParseError
+
+
+st.title("A Phishing Detection App")
+
+st.write("This is a Content-Based ML-Based app is developed as ")
+
+with st.expander("PROJECT DETAILS"):
+    st.subheader("Approach")
+    st.write("I used supervised learning to classify phishing and legitimate websites"
+             "Sckit-learn  was also used for the ML Models.")
+    st.write("For  this project, I created my own dataset and defined features based on some reviewed literature and manual inspection of some phishing websites")
+    st.write("The request library was used to scrap the webpages and BeautifulSoup module  was used to parse and extract features")
+    st.write("The  source code and datasets are available in the below  Github")
+    st.write("_https://www.github.com_")
+    
+    st.subheader("Datasets")
+    st.write(" I used _'phishtank.org'_ & _'tranco-list.eu'_ as data sources.")
+    st.write("Dataset contained a total of **_26584_** websites ===> **_16060_ - legitimate** websites | **_10524_** phishing websites")
+    
+    # ----- FOR THE PIE CHART ----- #
+    labels = 'phishing', 'legitimate'
+    phishing_rate = int(ml.phishing_df.shape[0] / (ml.phishing_df.shape[0] + ml.legitimate_df.shape[0]) * 100)
+    legitimate_rate = 100 - phishing_rate
+    sizes = [phishing_rate, legitimate_rate]
+    explode = (0.1, 0)
+    fig, ax = plt.subplots()
+    ax.pie(sizes, explode=explode, labels=labels, shadow=True, startangle=90, autopct='%1.1f%%')
+    ax.axis('equal')
+    st.pyplot(fig)
+    # ----- !!!!! ----- #
+    
+    st.write('Features + URL + Label ==> Dataframe')
+    st.markdown('label is 1 for phishing, 0 for legitimate')
+    number = st.slider("Select row number to display", 0, 100)
+    st.dataframe(ml.legitimate_df.head(number))
+
+
+    @st.cache_data
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(ml.df)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='phishing_legitimate_structured_data.csv',
+        mime='text/csv',
+    )
+
+    st.subheader('Features')
+    st.write('I used only content-based features. I didn\'t use url-based faetures like length of url etc.'
+             'Most of the features extracted using find_all() method of BeautifulSoup module after parsing html.')
+
+    st.subheader('Results')
+    st.write('I used 7 different ML classifiers of scikit-learn and tested them implementing k-fold cross validation.'
+             'Firstly obtained their confusion matrices, then calculated their accuracy, precision and recall scores.'
+             'Comparison table is below:')
+    st.table(ml.df_results)
+    st.write('NB --> Gaussian Naive Bayes')
+    st.write('SVM --> Support Vector Machine')
+    st.write('DT --> Decision Tree')
+    st.write('RF --> Random Forest')
+    st.write('AB --> AdaBoost')
+    st.write('NN --> Neural Network')
+    st.write('KN --> K-Neighbours')
+
+with st.expander('EXAMPLE PHISHING URLs:'):
+    st.write('_https://rtyu38.godaddysites.com/_')
+    st.write('_https://karafuru.invite-mint.com/_')
+    st.write('_https://defi-ned.top/h5/#/_')
+    st.caption('REMEMBER, PHISHING WEB PAGES HAVE SHORT LIFECYCLE! SO, THE EXAMPLES SHOULD BE UPDATED!')
+
+choice = st.selectbox("Please select your machine learning model",
+                 [
+                     'Gaussian Naive Bayes', 'Support Vector Machine', 'Decision Tree', 'Random Forest',
+                     'AdaBoost', 'Neural Network', 'K-Neighbours'
+                 ]
+                )
+
+model = ml.nb_model
+
+if choice == 'Gaussian Naive Bayes':
+    model = ml.nb_model
+    st.write('GNB model is selected!')
+elif choice == 'Support Vector Machine':
+    model = ml.svm_model
+    st.write('SVM model is selected!')
+elif choice == 'Decision Tree':
+    model = ml.dt_model
+    st.write('DT model is selected!')
+elif choice == 'Random Forest':
+    model = ml.rf_model
+    st.write('RF model is selected!')
+elif choice == 'AdaBoost':
+    model = ml.ab_model
+    st.write('AB model is selected!')
+elif choice == 'Neural Network':
+    model = ml.nn_model
+    st.write('NN model is selected!')
+else:
+    model = ml.kn_model
+    st.write('KN model is selected!')
+
+
+url = st.text_input('Enter the URL')
+# check the url is valid or not
+if st.button('Check URL'):
+    try:
+        response = re.get(url, verify=False, timeout=30)
+        if response.status_code != 200:
+            print(". HTTP connection was not successful for the URL: ", url)
+        else:
+            soup = BeautifulSoup(response.content, "html.parser")
+            vector = [fe.create_vector(soup)]  # it should be 2d array, so I added []
+            result = model.predict(vector)
+            if result[0] == 0:
+                st.success("This web page seems a legitimate!")
+                # st.balloons()
+            else:
+                st.warning("Attention! This web page is a potential PHISHING!")
+                # st.snow()
+                
+    except LocationParseError as e: 
+            print("URL:An error occurred while parsing the location:", e)
+            
+    except re.exceptions.RequestException as e:
+        print("--> ", e)
+    
+
